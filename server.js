b660b389bc26e00c51a9301ce95715fba4671778
gabietaylor <gabie.taylor@yahoo.com>
2017-07-30 08:05:40 -0400

    morgan body parser and public is required

--- server.js
+++ server.js
@@ -10,3 +10,13 @@ const Admins = require('./models/Users.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Morgan Logging
+app.use(logger('dev'));
+// Parse the data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+// Grad everything form public
+app.use(express.static('./public'));
