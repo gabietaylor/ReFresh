6d6bf61645cc36118adb910f684c3140a44d9cb8
gabietaylor <gabie.taylor@yahoo.com>
2017-08-12 18:32:22 -0400

    add misc code

--- app/components/Homepage.js
+++ app/components/Homepage.js
@@ -29,8 +29,8 @@ var Homepage = React.createClass({  render: function() {
             <p>ReFresh Yourself</p>
           </div>
           <div className="w3-display-topleft w3-container w3-xlarge">
-            <p><button onclick="document.getElementById('signup').style.display='block'" className="w3-button w3-black">signup</button></p>
-            <p><button onclick="document.getElementById('login').style.display='block'" className="w3-button w3-black">login</button></p>
+            <p><button onClick="document.getElementById('signup').style.display='block'" className="w3-button w3-black">SignUp</button></p>
+            <p><button onClick="document.getElementById('login').style.display='block'" className="w3-button w3-black">Login</button></p>
           </div>
           <div className="w3-display-bottomleft w3-container">
             <p className="w3-xlarge"> Find Fresh Food in Your Location </p>
@@ -41,7 +41,7 @@ var Homepage = React.createClass({  render: function() {
         <div id="signup" className="w3-modal">
           <div className="w3-modal-content w3-animate-zoom">
             <div className="w3-container w3-black w3-display-container">
-              <span onclick="document.getElementById('signup').style.display='none'" className="w3-button w3-display-topright w3-large">x</span>
+              <span onClick="document.getElementById('signup').style.display='none'" className="w3-button w3-display-topright w3-large">x</span>
               <h1>SignUp</h1>
             </div>
             <div className="w3-container">
@@ -74,7 +74,7 @@ var Homepage = React.createClass({  render: function() {
         <div id="login" className="w3-modal">
           <div className="w3-modal-content w3-animate-zoom">
             <div className="w3-container w3-black">
-              <span onclick="document.getElementById('login').style.display='none'" className="w3-button w3-display-topright w3-large">x</span>
+              <span onClick="document.getElementById('login').style.display='none'" className="w3-button w3-display-topright w3-large">x</span>
               <h1> Login </h1>
             </div>
             <div className="w3-container">
@@ -88,3 +88,39 @@ var Homepage = React.createClass({  render: function() {
 });
 
 module.exports = Homepage;
+
+/*import React from "react";
+class Profile extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            show:true
+        }
+    }
+    modalPopUp(){
+        console.log("Hello World!!");
+        if(this.state.show === true){
+            this.setState({show:false});
+        } else{
+            this.setState({show: true});
+        }
+        console.log(this.state.show);
+    }
+    render() {
+        var content = (
+            <div className="modal">
+                <h2>I am Esterling Accime</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet asperiores cupiditate dolor error ipsa ipsum laborum nihil sapiente voluptates! Consequuntur c
+                    ulpa cupiditate distinctio laborum minima nam, necessitatibus provident repellat?</p>
+            </div>
+        );
+        return (
+            <div>
+                <h1>Hello World!!!</h1>
+                {this.state.show === true ? content : <p>No it's not </p>}
+                <button onClick={this.modalPopUp.bind(this)}>Send Data</button>
+            </div>
+        );
+    }
+}
+export default Profile;*/
